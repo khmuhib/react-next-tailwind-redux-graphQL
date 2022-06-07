dcb8948b03b37769d9a986d49d9f3f2e2471addf
khmuhib <khmuhib2013@gmail.com>
2022-06-07 17:57:36 +0600

    First time upload

---
+++ pages/flexbox/flexBox.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import flexBoxStyles from "./flexBox.module.css";
+
+export default function flexBox() {
+  return (
+    <div class={flexBoxStyles.flex_box_main}>flexBox</div>
+  )
+}
---
+++ pages/walcart/walCart.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import stylesWalcart from './walCart.module.css'
+
+export default function walCart() {
+  return (
+    <div className={stylesWalcart.walcart_main}>walCart</div>
+  )
+}
